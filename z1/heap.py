311805d94251c985ccd59041d38740eb9ceef1eb
t0k4r <mi03to08@gmail.com>
2024-03-12 13:41:04 +0100

    faster heapsort

--- z1/heap.py
+++ z1/heap.py
@@ -4,19 +4,43 @@ import gen
 def sort(arr:list[int]):
     m = len(arr) - 1
     pom=0
+    levels=1
+    a=1
+    while 2**levels - 1 <= len(arr):
+        levels=levels+1
+
+    for i in range(levels):
+        while m > 0:
+            if m % 2 == 0:
+                if arr[m] > arr[(m - 1) // 2]:
+                    pom = arr[m]
+                    arr[m] = arr[(m - 1) // 2]
+                    arr[(m - 1) // 2] = pom
+                    
+            else:
+                if arr[m] > arr[(m - 1) // 2]:
+                    pom = arr[m]
+                    arr[m] = arr[(m - 1) // 2]
+                    arr[(m - 1) // 2] = pom
+                    
+            m -= 1
+
+def sort2(arr:list[int]):
+    m = len(arr) - 1
+    pom=0
     while m > 0:
         if m % 2 == 0:
             if arr[m] > arr[(m - 1) // 2]:
                 pom = arr[m]
                 arr[m] = arr[(m - 1) // 2]
                 arr[(m - 1) // 2] = pom
-                sort(arr)
+                sort2(arr)
         else:
             if arr[m] > arr[(m - 1) // 2]:
                 pom = arr[m]
                 arr[m] = arr[(m - 1) // 2]
                 arr[(m - 1) // 2] = pom
-                sort(arr)
+                sort2(arr)
         m -= 1
     
 def main():
