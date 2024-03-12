33668bc82a17df91112aec4ffa391853e5e86d08
t0k4r <mi03to08@gmail.com>
2024-03-12 13:55:46 +0100

    new as bad heapsort

--- z1/heap.py
+++ z1/heap.py
@@ -25,23 +25,68 @@ import gen
                     
 #             m -= 1
 
-def sort(arr:list[int]):
-    m = len(arr) - 1
+# def sort(arr:list[int]):
+#     m = len(arr) - 1
+#     pom=0
+#     while m > 0:
+#         if m % 2 == 0:
+#             if arr[m] > arr[(m - 1) // 2]:
+#                 pom = arr[m]
+#                 arr[m] = arr[(m - 1) // 2]
+#                 arr[(m - 1) // 2] = pom
+#                 sort(arr)
+#         else:
+#             if arr[m] > arr[(m - 1) // 2]:
+#                 pom = arr[m]
+#                 arr[m] = arr[(m - 1) // 2]
+#                 arr[(m - 1) // 2] = pom
+#                 sort(arr)
+#         m -= 1
+
+def heapSort(l):
+    m = len(l) - 1
     pom=0
-    while m > 0:
-        if m % 2 == 0:
-            if arr[m] > arr[(m - 1) // 2]:
-                pom = arr[m]
-                arr[m] = arr[(m - 1) // 2]
-                arr[(m - 1) // 2] = pom
-                sort(arr)
-        else:
-            if arr[m] > arr[(m - 1) // 2]:
-                pom = arr[m]
-                arr[m] = arr[(m - 1) // 2]
-                arr[(m - 1) // 2] = pom
-                sort(arr)
-        m -= 1
+    levels=1
+    a=1
+    while 2**levels - 1 <= len(l):
+        levels=levels+1
+
+    for i in range(levels):
+        while m > 0:
+            if m % 2 == 0:
+                if l[m] > l[(m - 1) // 2]:
+                    pom = l[m]
+                    l[m] = l[(m - 1) // 2]
+                    l[(m - 1) // 2] = pom
+                    
+            else:
+                if l[m] > l[(m - 1) // 2]:
+                    pom = l[m]
+                    l[m] = l[(m - 1) // 2]
+                    l[(m - 1) // 2] = pom
+                    
+            m -= 1
+        
+    return l
+
+
+def sort(arr: list[int]):
+    n = len(arr)
+    wynik=[]
+
+    g=len(arr)-1
+    heapSort(arr)
+    for _ in range(n):
+        pom=arr[0]
+        arr[0]=arr[g]
+        arr[g]=pom
+        g=g-1
+        wynik.append(arr[len(arr)-1])
+    
+        arr.pop(len(arr)-1)
+        heapSort(arr)
+    for e in wynik:
+        arr.append(e)
     
 def main():
     bench.debug(gen.Rand, sort, 10)
