0b4fd465543794e98c0629bedaff6fe3a0f04ac9
t0k4r <mi03to08@gmail.com>
2024-03-25 16:22:45 +0100

    sync

--- z2/avl.py
+++ z2/avl.py
@@ -1,8 +1,11 @@
 def main():
     t= Tree()
     t.build([8, 2, 5, 14, 10, 12, 13, 6, 9])
-    print(t)
-    pass
+    t.search(14)
+    t.deletesubtree(9)
+    inorder(t.root)
+
+
 
 class Node:
     def __init__(self, value) -> None:
@@ -22,6 +25,7 @@ class Node:
             else:
                 self.right.add(value)
 
+
 class Tree:
     def __init__(self) -> None:
         self.root: Node|None = None
@@ -31,12 +35,16 @@ class Tree:
         i = len(arr)//2
         mean = arr[i]
         self.add(mean)
-        larr = arr[0:i-1]
-        rarr = arr[i+1:len(arr)-1]
+        larr = arr[0:i]
+        rarr = arr[i+1:len(arr)]
         while len(larr) != 0:
-            self.add(larr.pop(len(larr)//2))
+            i = len(larr)//2
+            if len(larr)%2==0: i-=1
+            self.add(larr.pop(i))
         while len(rarr) != 0:
-            self.add(rarr.pop(len(rarr)//2))
+            i = len(rarr)//2
+            if len(rarr)%2==0: i-=1
+            self.add(rarr.pop(i))
 
     def add(self, value):
         if self.root == None:
@@ -51,8 +59,85 @@ class Tree:
             now = now.left 
         return now.value 
     
-    def minroute(self):
+    def delete(self, value):
         pass
+
+    def deletesubtree(self, value):
+        if self.root == None: raise Exception("404")
+        prev: Node|None = None
+        now = self.root
+        while now != None:
+            if now.value > value:
+                prev = now
+                now = now.left
+            elif now.value < value:
+                prev = now
+                now = now.right
+            else: 
+                if prev == None: 
+                    self.root = None
+                    return
+                if prev.left.value == now.value: #type: ignore
+                    prev.left = None
+                else:
+                    prev.right = None
+                return
+        raise Exception("404")
+    
+    def balance(self, value):
+        pass
+
+
+    def search(self, value):
+        if self.root == None: return False
+        rt = ""
+        now = self.root
+        while now != None:
+            rt += f"Node({now.value})"
+            if now.value > value:
+                rt+=f".left -> "
+                now = now.left
+            elif now.value < value:
+                rt+=f".right -> "
+                now = now.right
+            else: 
+                break
+        else:
+            return False
+        print(rt)
+        return True
+
+
+
+    def searchnode(self, value):
+        pass
+        if self.root == None: raise Exception("404")
+        now = self.root
+        while now != None:
+            if now.value > value:
+                now = now.left
+            elif now.value < value:
+                now = now.right
+            else: 
+                return now
+        raise Exception("404")
+
+    def searchlevel(self,value):
+        if self.root == None: return -1
+        rt = ""
+        now = self.root
+        i = 0
+        while now != None:
+            if now.value > value:
+                now = now.left
+            elif now.value < value:
+                now = now.right
+            else: 
+                break
+            i+=1
+        else:
+            return -1
+        return i
     
     def max(self):
         now = self.root
@@ -60,12 +145,46 @@ class Tree:
         while now.right != None:
             now = now.right 
         return now.value
-    
-    def maxroute(self):
-        pass
-    
+
     def height(self):
         pass
+
+def printlevel(root: Node|None, level: int, now: int):
+    if root == None: return
+    if now < level:
+        printlevel(root.left, level, now+1)
+        printlevel(root.right, level, now+1)
+    elif now == level:
+        print(root.value, end=", ")
+
+def preorder(root: Node|None):
+    if root == None:
+        return
+    print(root.value)
+    preorder(root.left)
+    preorder(root.right)
+
+def inorder(root: Node|None):
+    if root == None:
+        return
+    inorder(root.left)
+    print(root.value)
+    inorder(root.right)
+
+def inorderdesc(root: Node|None):
+    if root == None:
+        return
+    inorderdesc(root.right)
+    print(root.value)
+    inorderdesc(root.left)
+
+
+def postorder(root: Node|None):
+    if root == None:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.value)
 
     
 if __name__ == "__main__":
